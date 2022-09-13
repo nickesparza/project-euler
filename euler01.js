e6397faf55d8e149a235b56e5b79b928436acf39
nickesparza <nickesparza@gmail.com>
2022-09-13 13:38:27 -0500

    quick solve euler 1

---
+++ euler01.js
@@ -0,0 +1,13 @@
+// console.log('hello world')
+
+const multiples = () => {
+    let sum = 0
+    for (let i = 0; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i
+        }
+    }
+    return sum
+}
+
+console.log(multiples())
